ad808a9e48bd8b8116fa6839c382750595a2dac5
Jake Frederick <jake@jakefrederick.com>
2017-10-22 02:28:00 -0400

    made html audio unlock optional, updated overlay message

--- AudioUnlock.js
+++ AudioUnlock.js
@@ -1,10 +1,10 @@
-module.exports = function unlockAudio(context, completeCallback, addOverlay, params) {
+module.exports = function unlockAudio(context, completeCallback, unlockHTMLAudio, addOverlay, params) {
     var overlayContainer;
     var overlayTextNode;
     if(addOverlay) {
         overlayContainer = document.createElement('div');
-        overlayTextNode = document.createElement('span');
-        overlayTextNode.innerHTML = (params !== undefined && params.overlayText !== undefined) ? params.overlayText : 'tap to begin';
+        overlayTextNode = document.createElement('p');
+        overlayTextNode.innerHTML = (params !== undefined && params.overlayText !== undefined) ? params.overlayText : 'tap/click to begin<br />be sure your mute switch is off if you are on and iPhone';
         overlayTextNode.className = 'overlayTextNode';
         overlayContainer.appendChild(overlayTextNode);
         if(params !== undefined && params.overlayClassName !== undefined) {
@@ -20,13 +20,15 @@ module.exports = function unlockAudio(context, completeCallback, addOverlay, par
             overlayContainer.style.height = '100%';
             overlayContainer.style.color = '#ffffff';
             overlayContainer.style.fontFamily = 'Arial';
-            overlayContainer.style.fontSize = '2em';
+            overlayContainer.style.fontSize = '1.5em';
             overlayContainer.style.backgroundColor = 'rgba(0,0,0,0.5)';
         }
         document.body.insertBefore(overlayContainer, document.body.childNodes[0]);
     }
 
     var complete = () => {
+        document.body.removeEventListener('click', unlock, false);
+        document.body.removeEventListener('touchstart', unlock, false);
         if(overlayContainer !== undefined) {
             document.body.removeChild(overlayContainer);
             overlayContainer = null;
@@ -38,15 +40,13 @@ module.exports = function unlockAudio(context, completeCallback, addOverlay, par
     }
 
     var isWebAudioUnlocked = false;
-    var isHTMLAudioUnlocked = false;
+    var isHTMLAudioUnlocked = unlockHTMLAudio ? false : true;
 
     var unlock = () => {
         if(overlayTextNode !== undefined) {
             overlayTextNode.style.transition = 'all .25s ease-in-out';
-            overlayTextNode.style.opacity = '0';
+            overlayTextNode.style.opacity = '0.25';
         }
-        document.body.removeEventListener('click', unlock, false);
-        document.body.removeEventListener('touchend', unlock, false);
 
         if (context.state === 'suspended') {
             context.resume();
@@ -68,25 +68,31 @@ module.exports = function unlockAudio(context, completeCallback, addOverlay, par
         };
         source.start();
 
+        // The method below does not seem to reliably work; sometimes it does, sometimes it just plays a short snippet of the audio
+        // For this reason I have made its use optional
+        //
         // Unlock HTML5 Audio - load a data url of short silence and play it
         // This will allow us to play web audio when the mute toggle is on
-        var silenceDataURL = "data:audio/mp3;base64,//MkxAAHiAICWABElBeKPL/RANb2w+yiT1g/gTok//lP/W/l3h8QO/OCdCqCW2Cw//MkxAQHkAIWUAhEmAQXWUOFW2dxPu//9mr60ElY5sseQ+xxesmHKtZr7bsqqX2L//MkxAgFwAYiQAhEAC2hq22d3///9FTV6tA36JdgBJoOGgc+7qvqej5Zu7/7uI9l//MkxBQHAAYi8AhEAO193vt9KGOq+6qcT7hhfN5FTInmwk8RkqKImTM55pRQHQSq//MkxBsGkgoIAABHhTACIJLf99nVI///yuW1uBqWfEu7CgNPWGpUadBmZ////4sL//MkxCMHMAH9iABEmAsKioqKigsLCwtVTEFNRTMuOTkuNVVVVVVVVVVVVVVVVVVV//MkxCkECAUYCAAAAFVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV";
-        var tag = document.createElement("audio");
-        tag.controls = false;
-        tag.preload = "auto";
-        tag.loop = false;
-        tag.src = silenceDataURL;
-        tag.onended = function()
-        {
-            isHTMLAudioUnlocked = true;
-            if (isWebAudioUnlocked && isHTMLAudioUnlocked)
+        if(!isHTMLAudioUnlocked) {
+            var silenceDataURL = "data:audio/mp3;base64,//MkxAAHiAICWABElBeKPL/RANb2w+yiT1g/gTok//lP/W/l3h8QO/OCdCqCW2Cw//MkxAQHkAIWUAhEmAQXWUOFW2dxPu//9mr60ElY5sseQ+xxesmHKtZr7bsqqX2L//MkxAgFwAYiQAhEAC2hq22d3///9FTV6tA36JdgBJoOGgc+7qvqej5Zu7/7uI9l//MkxBQHAAYi8AhEAO193vt9KGOq+6qcT7hhfN5FTInmwk8RkqKImTM55pRQHQSq//MkxBsGkgoIAABHhTACIJLf99nVI///yuW1uBqWfEu7CgNPWGpUadBmZ////4sL//MkxCMHMAH9iABEmAsKioqKigsLCwtVTEFNRTMuOTkuNVVVVVVVVVVVVVVVVVVV//MkxCkECAUYCAAAAFVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV";
+            var tag = document.createElement("audio");
+            tag.controls = false;
+            tag.preload = "auto";
+            tag.loop = false;
+            tag.src = silenceDataURL;
+            tag.onended = function()
             {
-                //console.log("WebAudio unlocked and playable w/ mute toggled on!");
-                complete();
-            }
-        };
-        tag.play();
+                isHTMLAudioUnlocked = true;
+                if (isWebAudioUnlocked && isHTMLAudioUnlocked)
+                {
+                    //console.log("WebAudio unlocked and playable w/ mute toggled on!");
+                    complete();
+                }
+            };
+            tag.play();
+        }
+
     }
     document.body.addEventListener('click', unlock, false);
-    document.body.addEventListener('touchend', unlock, false);
+    document.body.addEventListener('touchstart', unlock, false);
 }
